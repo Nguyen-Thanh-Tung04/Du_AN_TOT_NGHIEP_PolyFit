6d1ca4eeec100a41019a715417b575fe8a270142
Nguyen-Thanh-Tung04 <thanhtung2432004@gmail.com>
2024-10-01 11:24:06 +0700

    Quản lý danh mục v2

--- app/Repositories/Interfaces/CategoryInterface.php
+++ app/Repositories/Interfaces/CategoryInterface.php
@@ -1,14 +1,24 @@
 <?php
+
 namespace App\Repositories\Interfaces;
 
 interface CategoryInterface
 {
     public function getAll();
     public function find($id);
-    public function pagination($columns, $condition, $perPage);
+    public function pagination(
+        array $column = ['*'], 
+        array $condition = [],
+        int $perpage = 1,
+        array $extend = [],
+    );
     public function create(array $payload = []);
     public function update(int $id = 0, array $payload = []);
     public function delete(int $id = 0);
-
+    public function updateByWhereIn(
+        string $whereInField = '',
+        array $whereIn = [],
+        array $payload = [],
+    );
     // Add other interface methods as needed
 }
