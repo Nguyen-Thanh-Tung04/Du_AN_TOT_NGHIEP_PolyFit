f0e76a17db7496afec6f7d8758b50256b7dec5bd
trinhhuong2004 <huongtb.jvb@gmail.com>
2024-12-20 18:29:35 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Chat') }}</title>
+    <title>Web Thời Trang FPoly Hà Nội</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
